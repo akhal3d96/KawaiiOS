ff1fdf70a0af9adf87df5428c9562700862ebff2
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2018-09-26 23:14:33 +0200

    Reorganized the bootloader

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -14,7 +14,7 @@ boot:
     ; mov bp, 0x9000          ; Set  the  stack.
     ; mov sp, bp
 
-    mov SI, Stage0
+    mov SI, Stage0_msg
     call PrintString
 
     call LoadKernel
@@ -26,12 +26,14 @@ boot:
 
 ; load_kernel
 LoadKernel:
-    mov SI, KernelStatus    ; Print a message  to say we are  loading  the  kernel
+    mov SI, KernelStatus        ; Print a message  to say we are  loading  the  kernel
     call  PrintString
-    mov bx, KERNEL_OFFSET      ; Set -up  parameters  for  our  disk_load  routine , so
-    mov dh, 20                  ; that we load  the  first  15  sectors (excluding
-    mov dl, [BOOT_DRIVE]       ; the  boot  sector) from  the  boot  disk (i.e.  our
-    call  disk_load               ;   kernel  code) to  address  KERNEL_OFFSET
+    
+    mov bx, KERNEL_OFFSET       ; Set -up  parameters  for  our  disk_load  routine , so
+    mov dh, 20                  ; that we load  the  first  20  sectors (excluding
+    mov dl, [BOOT_DRIVE]        ; the  boot  sector) from  the  boot  disk (i.e.  our
+    
+    call  DiskLoad              ; kernel  code) to  address  KERNEL_OFFSET
 
 EnableA20:
     pusha
@@ -40,25 +42,28 @@ EnableA20:
     out 0x92, al
     popa
     ret
+
 ; load DH  sectors  to ES:BX from  drive  DL
-disk_load:
-    push dx           ; Store  DX on  stack  so  later  we can  recall
-    ; how  many  sectors  were  request  to be read ,
-    ; even if it is  altered  in the  meantime
-    mov ah, 0x02     ; BIOS  read  sector  function
-    mov al, dh       ; Read DH  sectors
-    mov ch, 0x00     ; Select  cylinder 0
-    mov dh, 0x00     ; Select  head 0
-    mov cl, 0x02     ; Start  reading  from  second  sector (i.e.
-    ; after  the  boot  sector)
-    int 0x13          ; BIOS  interrupt
-    jc  disk_error    ; Jump if error (i.e.  carry  flag  set)
-    pop dx            ; Restore  DX from  the  stack
-    cmp dh, al       ; if AL (sectors  read) != DH (sectors  expected)
-    jne  disk_error   ;    display  error  message
+DiskLoad:
+    push dx         ; Store  DX on  stack  so  later  we can  recall
+                    ; how  many  sectors  were  request  to be read ,
+                    ; even if it is  altered  in the  meantime
+    
+    mov ah, 0x02    ; BIOS  read  sector  function
+    mov al, dh      ; Read DH  sectors
+    mov ch, 0x00    ; Select  cylinder 0
+    mov dh, 0x00    ; Select  head 0
+    
+    mov cl, 0x02    ; Start  reading  from  second  sector (i.e.
+                    ; after  the  boot  sector)
+    int 0x13        ; BIOS  interrupt
+    jc  DiskError  ; Jump if error (i.e.  carry  flag  set)
+    pop dx          ; Restore  DX from  the  stack
+    cmp dh, al      ; if AL (sectors  read) != DH (sectors  expected)
+    jne  DiskError ; display  error  message
     ret
 
-disk_error :
+DiskError:
     mov SI, DISK_ERROR_MSG
     call  PrintString
     jmp $
@@ -66,23 +71,10 @@ disk_error :
 %include "io.asm"
 %include "gdt.asm"
 %include "protected-mode.asm"
-%include "io-vga.asm"
+%include "io-pm.asm"
 
-bits 32 
-
-BEGIN_PM:
-    mov ebx , Stage1
-    call  print_string_pm    ; Use  our 32-bit  print  routine.
-    ; jmp $
-    call EnableA20
-    ; mov ebx, A20
-    ; call print_string_pm
-    call KERNEL_OFFSET
-    jmp $
-   ; Hang.
- 
-Stage0 db 'KawaiiOS Bootloader (Stage 0)', 0x0D, 0xA, 0	;HelloWorld string ending with 0
-Stage1 db 'KawaiiOS Bootloader (Stage 1)', 0x0D, 0xA, 0
+Stage0_msg db 'KawaiiOS Bootloader (Stage 0)', 0x0D, 0xA, 0
+Stage1_msg db 'KawaiiOS Bootloader (Stage 1)', 0x0D, 0xA, 0
 
 A20 db 'A20 line was enabled', 0
 
--- bootloader/protected-mode.asm
+++ bootloader/protected-mode.asm
@@ -2,20 +2,20 @@ bits  16
 ; Switch  to  protected  mode
 SwitchToPM:
     ; cli                        ; We must  switch  of  interrupts  until  we have
-    ; set -up the  protected  mode  interrupt  vector
-    ; otherwise  interrupts  will  run  riot
-    lgdt [gdt_descriptor]   ; Load  our  global  descriptor  table , which  defines
-    ; the  protected  mode  segments (e.g.  for  code  and  data)
-    mov eax , cr0             ; To make  the  switch  to  protected  mode , we set
-    or eax , 0x1               ; the  first  bit of CR0 , a control  register
+                                 ; set -up the  protected  mode  interrupt  vector
+                                 ; otherwise  interrupts  will  run  riot
+    lgdt [gdt_descriptor]        ; Load  our  global  descriptor  table , which  defines
+                                 ; the  protected  mode  segments (e.g.  for  code  and  data)
+    mov eax , cr0                ; To make  the  switch  to  protected  mode , we set
+    or eax , 0x1                 ; the  first  bit of CR0 , a control  register
     mov cr0 , eax
-    jmp  CODE_SEG:init_pm    ; Make a far  jump (i.e. to a new  segment) to our 32-bit
-    ; code.   This  also  forces  the  CPU to  flush  its  cache  of
-    ; pre -fetched  and real -mode  decoded  instructions , which  can
-    ; cause  problems.
+    jmp  CODE_SEG:InitPM         ; Make a far  jump (i.e. to a new  segment) to our 32-bit
+                                 ; code.   This  also  forces  the  CPU to  flush  its  cache  of
+                                 ; pre -fetched  and real -mode  decoded  instructions , which  can
+                                 ; cause  problems.
 bits  32
 ; Initialise  registers  and  the  stack  once in PM.
-init_pm:
+InitPM:
     mov ax, DATA_SEG          ; Now in PM, our  old  segments  are  meaningless ,
     mov ds, ax                ; so we  point  our  segment  registers  to the
     mov ss, ax                ; data  selector  we  defined  in our  GDT
@@ -24,4 +24,18 @@ init_pm:
     mov gs, ax
     mov ebp , 0x90000         ; Update  our  stack  position  so it is  right
     mov esp , ebp             ; at the  top of the  free  space.
-    call  BEGIN_PM            ; Finally , call  some well -known  labe
+    call  BeginPM             ; Finally , call  some well -known  labe
+
+BeginPM:
+    mov ebx , Stage1_msg
+    call  PrintStringPM       ; Use  our 32-bit  print  routine.
+    
+    
+    call EnableA20
+    mov ebx, A20
+    call PrintStringPM
+    
+    call KERNEL_OFFSET        ; Go to the place where we loaded the kernel
+    
+    jmp $                     ; Hang there in case of an error.
+ 
